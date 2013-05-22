9bd22a529ddda17b4059c941276ed35409ba301c
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2013-05-22 22:44:54 +0200

    Add isoparametric interpolation polynomial generator functions
    The generic functions produce a variety of isoparametric transformations
    of any requested degree. Currently they still use the string monomials,
    but in future the direct use of the exponents may speed up the
    transfomations.

--- pyformex/plugins/isopar.py
+++ pyformex/plugins/isopar.py
@@ -47,10 +47,94 @@ def evaluate(atoms,x,y=0,z=0):
     return aa
 
 
+def exponents(n,layout='lag'):
+    """Create tuples of polynomial exponents.
+
+    This function creates the exponents of polynomials in 1 to 3 dimensions
+    which can be used to construct interpolation function over lagrangian,
+    triangular or serendipity grids.
+
+    Parameters:
+
+    - `n`: a tuple of 1 to 3 integers, specifying the number of points in
+      the x up to z directions.
+    - `layout`: string, specifying the layout of grid and the selection of
+      monomials to be used. Should be one of 'lagrangian', 'triangular',
+      'serendipity' or 'border'. The string can be abbreviated to its
+      first 3 characters.
+
+    Returns an integer array of shape (ndim,npoints), where ndim = len(n)
+    and npoints depends on the layout:
+
+    - lagrangian: npoints = prod(n). The point layout is a rectangular
+      lagrangian grid form by n[i] points in direction i. As an example,
+      specifying n=(3,2) uses a grid of 3 points in x-direction and 2 points
+      in y-direction.
+    - triangular: requires that all values in n are equal. For ndim=2, the
+      number of points is n*(n+1)/2.
+    - border: this is like the lagrangian grid with all internal points
+      removed. For ndim=2, we have npoints = 2 * sum(n) - 4. For ndim=3 we
+      have npoints = 2 * sum(nx*ny+ny*nz+nz*nx) - 4 * sum(n) + 8.
+      Thus n=(3,3,3) will yield 2*3*3*3 - 4*(3+3+3) + 8 = 26
+    - serendipity:
+
+    """
+    n = checkArray(n,(-1,),'i')
+    ndim = n.shape[0]
+    if ndim < 1 or ndim > 3:
+        raise RuntimeError,"Expected a 1..3 length tuple"
+
+    layout = layout[:3]
+    if layout == 'tri':
+        if not (n == n[0]).all():
+            raise RuntimeError,"For triangular grids, all axes should have the same number of points"
+
+
+    # First create the full lagrangian set
+    exp = indices(n).reshape(ndim,-1).transpose()
+
+    if layout == 'lag':
+        # We're done
+        pass
+    elif layout == 'tri':
+        # Select by total maximal degree
+        deg = n[0]-1
+        ok = exp.sum(axis=-1) <= deg
+        exp = exp[ok]
+    elif layout == 'bor':
+        # Select if any value is not higher than 1
+        ok = (exp <= 1).any(axis=-1)
+        exp = exp[ok]
+
+    return exp
+
+
+def monomial(exp,symbol='xyz'):
+    """Compute the monomials for the given exponents
+
+    exp is a tuple of 1 to 3 exponents
+    """
+    factor = lambda sym,exp: '1' if exp == 0 else sym if exp == 1 else sym+'**'+str(exp)
+    factors = [ factor(symbol[i],j) for i,j in enumerate(exp) ]
+    # Join and sanitize (note we do not have '**1')
+    return '*'.join(factors).replace('1*','').replace('*1','')
+
+
+def monomials(n,layout='lag'):
+    """Create the list of monomials for an element
+
+    Returns a tuple (ndim,list of monomials), like the values in
+    isodata below
+    """
+    exp = exponents(n,layout)
+    mon = map(monomial,exp)
+    return (len(n),mon)
+
+
 class Isopar(object):
     """A class representing an isoparametric transformation
 
-        type is one of the keys in Isopar.isodata
+        eltype is one of the keys in Isopar.isodata
         coords and oldcoords can be either arrays, Coords or Formex instances,
         but should be of equal shape, and match the number of atoms in the
         specified transformation type
@@ -67,7 +151,9 @@ class Isopar(object):
 
     """
 
-    # REM: we should create a function to produce these data
+    # REM: We started to create some more general functions to generate
+    # interpolation polynomials (see above)
+    # As a result, the comments below is somewhat obsolete
 
     # LAGRANGIAN : 1,2,3 dim (LINE, QUAD, HEX)
     # TRIANGLE : 2,3 dim (TRI,TET)
@@ -89,73 +175,41 @@ class Isopar(object):
     #   'lag-i-j-k'
 
 
+    #
+    # Currently, we still use a static list of string monomials
+    # This will be replaced later
+    #
     isodata = {
-        'line2' : (1, ('1','x')),
-        'line3' : (1, ('1','x','x*x')),
-        'line4' : (1, ('1','x','x**2','x**3')),
-        'tri3'  : (2, ('1','x','y')),
-        'tri3'  : (2, ('1','x','y')),
-        'tri6'  : (2, ('1','x','y','x*x','y*y','x*y')),
-        'tri10' : (2, ('1','x','y','x*x','y*y','x*y',
-                       'x*x*x', 'x*x*y', 'x*y*y', 'y*y*y')),
-        'quad4' : (2, ('1','x','y','x*y')),
-        'quad8' : (2, ('1','x','y','x*x','y*y','x*y','x*x*y','x*y*y')),
-        'quad9' : (2, ('1','x','y','x*x','y*y','x*y','x*x*y','x*y*y',
-                       'x*x*y*y')),
-        'quad12': (2, ('1','x','y','x*x','x*y','y*y',
-                       'x*x*x','x*x*y','x*y*y','y*y*y',
-                       'x*x*x*y','x*y*y*y')),
+        'line2' : monomials((2,)),
+        'line3' : monomials((3,)),
+        'line4' : monomials((4,)),
+        'tri3'  : monomials((2,2),'tri'),
+        'tri6'  : monomials((3,3),'tri'),
+        'tri10' : monomials((4,4),'tri'),
+        'quad4' : monomials((2,2)),
+        'quad9' : monomials((3,3)),
+        'quad16': monomials((4,4)),
+        'quad8' : monomials((3,3),'bor'),
+        'quad12': monomials((4,4),'bor'),
+        'tet4'  : monomials((2,2,2),'tri'),
+        'tet10' : monomials((3,3,3),'tri'),
+        'hex8'  : monomials((2,2,2)),
+        'hex27' : monomials((3,3,3)),
+        'hex36' : monomials((3,3,4)),
+        'hex64' : monomials((4,4,4)),
+        # Serendipity still needs to be done
         'quad13': (2, ('1','x','y','x*x','x*y','y*y',
                        'x*x*x','x*x*y','x*y*y','y*y*y',
                        'x*x*x*y','x*x*y*y','x*y*y*y')),
-        'quad16': (2, ('1','x','y','x*x','x*y','y*y',
-                       'x*x*x','x*x*y','x*y*y','y*y*y',
-                       'x*x*x*y','x*x*y*y','x*y*y*y',
-                       'x*x*x*y*y','x*x*y*y*y','x*x*x*y*y*y')),
-        'tet4'  : (3, ('1','x','y','z')),
-        'tet10' : (3, ('1','x','y','z','x*x','y*y','z*z','x*y','x*z','y*z')),
-        'hex8'  : (3, ('1','x','y','z','x*y','x*z','y*z','x*y*z')),
         'hex20' : (3, ('1','x','y','z','x*x','y*y','z*z','x*y','x*z','y*z',
                        'x*x*y','x*x*z','x*y*y','y*y*z','x*z*z','y*z*z','x*y*z',
                        'x*x*y*z','x*y*y*z','x*y*z*z')),
-        'hex27' : (3, ('1','x','y','z','x*x','y*y','z*z','x*y','x*z','y*z',
-                       'x*x*y','x*x*z','x*y*y','y*y*z','x*z*z','y*z*z','x*y*z',
-                       'x*x*y*y','x*x*z*z','y*y*z*z','x*x*y*z','x*y*y*z',
-                       'x*y*z*z',
-                       'x*x*y*y*z','x*x*y*z*z','x*y*y*z*z',
-                       'x*x*y*y*z*z')),
-        # quadratic in x,y, cubic in z
-        'hex36' : (3, ('1','x','y','z','x*x','y*y','z*z','x*y','x*z','y*z',
-                       'x*x*y','x*x*z','x*y*y','y*y*z','x*z*z','y*z*z','x*y*z',
-                       'x*x*y*y','x*x*z*z','y*y*z*z','x*x*y*z','x*y*y*z',
-                       'x*y*z*z',
-                       'x*x*y*y*z','x*x*y*z*z','x*y*y*z*z',
-                       'x*x*y*y*z*z',
-                       'z*z*z','x*z*z*z','y*z*z*z',
-                       'x*x*z*z*z','y*y*z*z*z','x*y*z*z*z',
-                       'x*x*y*z*z*z','x*y*y*z*z*z','x*x*y*y*z*z*z')),
-        'hex64': (3, ('1','x','y','z','x*x','y*y','z*z','x*y','x*z','y*z',
-                      'x*x*y','x*x*z','x*y*y','y*y*z','x*z*z','y*z*z','x*y*z',
-                      'x*x*x','y*y*y','z*z*z',
-                      'x*x*y*y','x*x*z*z','y*y*z*z','x*x*y*z','x*y*y*z',
-                      'x*y*z*z','x*x*x*y','x*x*x*z','x*y*y*y','y*y*y*z',
-                      'x*z*z*z','y*z*z*z',
-                      'x*x*x*y*y','x*x*x*z*z','x*x*y*y*y','y*y*y*z*z',
-                      'z*z*z*x*x','z*z*z*y*y',
-                      'x*x*x*y*z','x*y*y*y*z','z*z*z*x*y',
-                      'x*x*y*y*z','x*x*y*z*z','x*y*y*z*z',
-                      'x*x*x*y*y*y','x*x*x*z*z*z','y*y*y*z*z*z',
-                      'x*x*x*y*y*z','x*x*x*y*z*z','y*y*y*x*x*z','y*y*y*x*z*z',
-                      'z*z*z*x*x*y','z*z*z*x*y*y','x*x*y*y*z*z',
-                      'x*x*x*y*y*y*z','x*x*x*y*z*z*z','y*y*y*x*z*z*z',
-                      'x*x*x*y*y*z*z','y*y*y*x*x*z*z','z*z*z*x*x*y*y',
-                      'x*x*x*y*y*y*z*z','x*x*x*z*z*z*y*y','y*y*y*z*z*z*x*x',
-                      'x*x*x*y*y*y*z*z*z')),
         }
 
 
     def __init__(self,eltype,coords,oldcoords):
         """Create an isoparametric transformation.
+
         """
         ndim,atoms = Isopar.isodata[eltype]
         coords = coords.view().reshape(-1,3)
