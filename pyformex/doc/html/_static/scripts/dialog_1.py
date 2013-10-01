18c4948642ebde1a05bec353f643c78a1df31dd5
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2013-10-01 20:14:38 +0200

    Rebuild documentation
    New documentation available from local doc menu.

--- pyformex/doc/html/_static/scripts/dialog_1.py
+++ pyformex/doc/html/_static/scripts/dialog_1.py
@@ -1,11 +1,11 @@
-    res = askItems([
-        dict(name='m',value=10,text='number of modules in axial direction'),
-        dict(name='n',value=8,text='number of modules in tangential direction'),
-        dict(name='r',value=10.,text='barrel radius'),
-        dict(name='a',value=180.,text='barrel opening angle'),
-        dict(name='l',value=30.,text='barrel length'),
-        ])
-    if not res:
-        return
+res = askItems([
+    dict(name='m',value=10,text='number of modules in axial direction'),
+    dict(name='n',value=8,text='number of modules in tangential direction'),
+    dict(name='r',value=10.,text='barrel radius'),
+    dict(name='a',value=180.,text='barrel opening angle'),
+    dict(name='l',value=30.,text='barrel length'),
+    ])
+if not res:
+    exit()
 
-    globals().update(res)
+globals().update(res)
