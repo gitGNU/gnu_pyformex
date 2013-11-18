gitGNU/gnu_pyformex

commit 0b4b6d9e1166e50ff9d1c09ab8dc2bdf4cf2f935

gtsset: check for specific errors in P.err
Screen P.err resulting from gtsset in order to find specific errors.
Added an error case:
intersection of surf1 and surf2 is not a closed curve.
This is always the case when a boolean is performed on
not intersecting surfaces. In this case instead of an raising an error,
 a string is returned in order to alert the user. As pointed out
in sr #108442 there can always be an other reason for this error.
The user should recongnize this specific event.

pyformex/plugins/pyformex_gts.py
@@ -127,6 +127,11 @@ def gtsset(self,surf,op,filt='',ext='.tmp',curve=False,check=False,verbose=False
     os.remove(tmp1)
     if P.sta or verbose:
         pf.message(P.out)
+    err=[l for l in P.err.split('\n') if l[0:1]!='#' and l!='']#remove empty and commented lines from P.err
+    if len(err)>0:#here some specific errors could be screened from P.err
+        if err[0]=='gtsset: the intersection of `%s\' and `%s\' is not a closed curve'%(tmp, tmp1):
+            pf.message(err[0])
+            return 'notAClosedCurve'
     pf.message("Reading result from %s" % tmp2)
     if curve:
         res = read_gts_intersectioncurve(tmp2)

commit 03c9c4b1c27706cf6f332d16f34b0140e48c1470

Reverse commit 0b4b6d9e
The error parsing introduced by 0b4b6d9e has been removed, as it is
considered needless cruft. It does not make sense to scan all error
messages and replace them with another error message. The best thing
we can do on an error is either
- let the thing continue until it hits an exception. The user can capture
  it and act accordingly
- capture the error ourself and give information to the user by printing
  some message and/or returning a special error code.
We just have to make sure we do not return valid but incorrect results.

Therefore I throw out the error detection code from gtsset.
I did not reverse to the original (which probably raised an exception),
but let the function print the error and return with a None value instead
of the TriSurface or curve (maybe we should change the returned curve
to a PolyLine instead of a Formex?).

pyformex/plugins/pyformex_gts.py
@@ -31,7 +31,7 @@ import pyformex as pf
 from coords import *
 import utils
 import os
-from plugins.trisurface import TriSurface,read_gts_intersectioncurve
+from plugins.trisurface import TriSurface
 
 if not utils.hasExternal('gts-bin'):
     utils.warn("I am missing the gts binary programs on your system.\nTherefore, some surface operations will not be available or fail.\n\nOn Debian, you can install the missing programs with `apt-get install libgts-bin`.\n")
@@ -44,6 +44,22 @@ if not utils.hasExternal('gts-extra'):
 #   in Debian package: stl2gts gts2stl gtscheck
 #   not in Debian package: gtssplit gtscoarsen gtsrefine gtssmooth gtsinside
 #
+
+
+def read_gts_intersectioncurve(fn):
+    import re
+    from formex import Formex
+    RE = re.compile("^VECT 1 2 0 2 0 (?P<data>.*)$")
+    r = []
+    for line in open(fn,'r'):
+        m = RE.match(line)
+        if m:
+            r.append(m.group('data'))
+    nelems = len(r)
+    x = fromstring('\n'.join(r),sep=' ').reshape(-1,2,3)
+    F = Formex(x)
+    return F
+
 
 def boolean(self,surf,op,check=False,verbose=False):
     """Perform a boolean operation with another surface.
@@ -103,7 +119,8 @@ def gtsset(self,surf,op,filt='',ext='.tmp',curve=False,check=False,verbose=False
 
     - curve: if True, an intersection curve is computed, else the surface.
 
-    Returns the name of the (temporary) results file.
+    Returns the resulting TriSurface (curve=False), or a plex-2 Formex
+    (curve=True), or None if the input surfaces do not intersect.
     """
     op = {'+':'union', '-':'diff', '*':'inter'}[op]
     options = ''
@@ -127,11 +144,9 @@ def gtsset(self,surf,op,filt='',ext='.tmp',curve=False,check=False,verbose=False
     os.remove(tmp1)
     if P.sta or verbose:
         pf.message(P.out)
-    err=[l for l in P.err.split('\n') if l[0:1]!='#' and l!='']#remove empty and commented lines from P.err
-    if len(err)>0:#here some specific errors could be screened from P.err
-        if err[0]=='gtsset: the intersection of `%s\' and `%s\' is not a closed curve'%(tmp, tmp1):
-            pf.message(err[0])
-            return 'notAClosedCurve'
+    if P.sta:
+        pf.message(P.err)
+        return None
     pf.message("Reading result from %s" % tmp2)
     if curve:
         res = read_gts_intersectioncurve(tmp2)

pyformex/plugins/trisurface.py
@@ -159,20 +159,6 @@ def read_stl(fn,intermediate=None):
         return read_off(ofn)
 
 
-def read_gts_intersectioncurve(fn):
-    import re
-    RE = re.compile("^VECT 1 2 0 2 0 (?P<data>.*)$")
-    r = []
-    for line in open(fn,'r'):
-        m = RE.match(line)
-        if m:
-            r.append(m.group('data'))
-    nelems = len(r)
-    x = fromstring('\n'.join(r),sep=' ').reshape(-1,2,3)
-    F = Formex(x)
-    return F
-
-
 
 # Surface characteristics
 