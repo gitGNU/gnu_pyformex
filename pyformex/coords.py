409ce9101c3056318d57772e54fc7012d477ce01
Francesco <francesco.iannaccone@ugent.be>
2014-07-08 12:43:17 +0200

    Added coords.positionCoordsObj function and modified coords.sweepCoords to call the prevoius one.
    positionCoordsObj position a list of coords objects along a Curve path.
    sweepCoords, now calls this function, as before the Coords object is
    passed as input, but it is replicated and evetually sacled before being
    passed to positionCoordsObj. This should solve bug #42441.

--- pyformex/coords.py
+++ pyformex/coords.py
@@ -2247,12 +2247,10 @@ def align(L,align,offset=[0., 0., 0.]):
     return r
 
 
-def sweepCoords(self,path,origin=[0., 0., 0.],normal=0,upvector=2,avgdir=False,enddir=None,scalex=None,scaley=None,scalez=None):
-    """ Sweep a Coords object along a path, returning a series of copies.
+def positionCoordsObj(objects,path,normal=0,upvector=2,avgdir=False,enddir=None):
+    """ Position a list of Coords objects along a path.
 
-    origin and normal define the local path position and direction on the mesh.
-
-    At each point of the curve, a copy of the Coords object is created, with
+    At each point of the curve, ao of the Coords object is created, with
     its origin in the curve's point, and its normal along the curve's direction.
     In case of a PolyLine, directions are pointing to the next point by default.
     If avgdir==True, average directions are taken at the intermediate points
@@ -2266,12 +2264,7 @@ def sweepCoords(self,path,origin=[0., 0., 0.],normal=0,upvector=2,avgdir=False,e
     If the curve is closed, endpoints are treated as any intermediate point,
     and the user should normally not specify enddir.
 
-    At each point of the curve, the original Coords object can be scaled in x
-    and y direction by specifying scalex and scaley. The number of values
-    specified in scalex and scaly should be equal to the number of points on
-    the curve.
-
-    The return value is a sequence of the transformed Coords objects.
+    The return value is a sequence of the repositioned Coords objects.
     """
     points = path.coords
     if isinstance(avgdir, bool):
@@ -2299,33 +2292,56 @@ def sweepCoords(self,path,origin=[0., 0., 0.],normal=0,upvector=2,avgdir=False,e
     directions = normalize(directions)
 
     if isinstance(normal, int):
-        normal = unitVector(normal)
-
+        normal = Coords(unitVector(normal))
+    
     if isinstance(upvector, int):
         upvector = Coords(unitVector(upvector))
+        
+    sequence = [ object.rotate(vectorRotation(normal, d, upvector)).translate(p)
+                 for object, d, p in zip(objects, directions, points)
+                 ]
 
+    return sequence
+
+
+def sweepCoords(self,path,origin=[0.,0.,0.],scalex=None,scaley=None,scalez=None,**kwargs):
+    """ Sweep a Coords object along a path, returning a series of copies.
+
+    origin and normal define the local path position and direction on the mesh.
+
+    At each point of the curve, a copy of the Coords object is created, with
+    its origin in the curve's point.
+
+    At each point of the curve, the original Coords object can be scaled in x
+    and y direction by specifying scalex and scaley. The number of values
+    specified in scalex and scaly should be equal to the number of points on
+    the curve.
+    All the kwargs parameters are used to set `positionCoordsObj` function.
+
+    The return value is a sequence of the transformed Coords objects.
+    """
+    
     if scalex is not None:
-        if len(scalex) != points.shape[0]:
+        if len(scalex) != path.ncoords():
             raise ValueError("The number of scale values in x-direction differs from the number of copies that will be created.")
     else:
-        scalex = ones(points.shape[0])
+        scalex = ones(path.ncoords())
 
     if scaley is not None:
-        if len(scaley) != points.shape[0]:
+        if len(scaley) != path.ncoords():
             raise ValueError("The number of scale values in y-direction differs from the number of copies that will be created.")
     else:
-        scaley = ones(points.shape[0])
+        scaley = ones(path.ncoords())
 
     if scalez is not None:
-        if len(scalez) != points.shape[0]:
+        if len(scalez) != path.ncoords():
             raise ValueError("The number of scale values in y-direction differs from the number of copies that will be created.")
     else:
-        scalez = ones(points.shape[0])
-
+        scalez = ones(path.ncoords())
+    
     base = self.translate(-Coords(origin))
-    sequence = [ base.scale([scx, scy, scz]).rotate(vectorRotation(normal, d, upvector)).translate(p)
-                 for scx, scy, scz, d, p in zip(scalex, scaley, scalez, directions, points)
-                 ]
+    sequence = [ base.scale([scx, scy, scz]) for scx, scy, scz in zip(scalex, scaley, scalez) ]
+    sequence = positionCoordsObj(sequence,path,**kwargs)
 
     return sequence
 
