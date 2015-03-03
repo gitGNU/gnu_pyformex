1aeb30c450d0eab4d2bc2436f69b8f8bd9a7d4e5
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2015-03-03 17:57:29 +0100

    Improve example Clip for app mode
    Move all functional code in the run function, to avoid running
    on loading the example.

--- pyformex/examples/Clip.py
+++ pyformex/examples/Clip.py
@@ -36,63 +36,67 @@ from pyformex.gui.draw import *
 from pyformex import zip
 from pyformex import simple
 
-# A square domain of triangles
-n = 16
-F = simple.rectangle(n,n,diag='d')
-
-# Novation (Spots)
-m = 4
-h = 0.15*n
-r = n/m
-s = n/r
-a = [ [r*i, r*j, h]  for j in range(1, s) for i in range(1, s) ]
-
-for p in a:
-    F = F.bump(2, p, lambda x:exp(-0.75*x), [0, 1])
-
-
-# Define a plane
-plane_p = [3.2, 3.0, 0.0]
-plane_n = [2.0, 1.0, 0.0]
-#compute number of nodes above/below the plane
-dist = F.distanceFromPlane(plane_p, plane_n)
-above = (dist>0.0).sum(-1)
-below = (dist<0.0).sum(-1)
-
-# Define a line by a point and direction
-line_p = [0.0, 0.0, 0.0]
-line_n = [1., 1., 1./3]
-d = F.distanceFromLine(line_p, line_n)
-# compute number of nodes closer that 2.2 to line
-close = (d < 2.2).sum(-1)
-
-
-
-sel = [ F.test(nodes=0, dir=0, min=1.5, max=3.5),
-        F.test(nodes=[0, 1], dir=0, min=1.5, max=3.5),
-        F.test(nodes=[0, 1, 2], dir=0, min=1.5, max=3.5),
-        F.test(nodes='all', dir=1, min=1.5, max=3.5),
-        F.test(nodes='any', dir=1, min=1.5, max=3.5),
-        F.test(nodes='none', dir=1, min=1.5),
-        (above > 0) * (below > 0 ),
-        close == 3,
-        ]
-
-txt = [ 'First node has x between 1.5 and 3.5',
-        'First two nodes have x between 1.5 and 3.5',
-        'First 3 nodes have x between 1.5 and 3.5',
-        'All nodes have y between 1.5 and 3.5',
-        'Any node has y between 1.5 and 3.5',
-        'No node has y larger than 1.5',
-        'Touching the plane P = [3.2,3.0,0.0], n = [2.0,1.0,0.0]',
-        '3 nodes close to line through [0.0,0.0,0.0] and [1.0,1.0,1.0]',
-        ]
-
 
 def run():
     resetAll()
     setDrawOptions({'clear':True, 'shrink':True})
     delay(1)
+
+
+    # A square domain of triangles
+    n = 16
+    F = simple.rectangle(n,n,diag='d')
+
+    # Novation (Spots)
+    m = 4
+    h = 0.15*n
+    r = n/m
+    s = n/r
+    a = [ [r*i, r*j, h]  for j in range(1, s) for i in range(1, s) ]
+
+    for p in a:
+        F = F.bump(2, p, lambda x:exp(-0.75*x), [0, 1])
+
+
+    # Define a plane
+    plane_p = [3.2, 3.0, 0.0]
+    plane_n = [2.0, 1.0, 0.0]
+    #compute number of nodes above/below the plane
+    dist = F.distanceFromPlane(plane_p, plane_n)
+    above = (dist>0.0).sum(-1)
+    below = (dist<0.0).sum(-1)
+
+    # Define a line by a point and direction
+    line_p = [0.0, 0.0, 0.0]
+    line_n = [1., 1., 1./3]
+    d = F.distanceFromLine(line_p, line_n)
+    # compute number of nodes closer that 2.2 to line
+    close = (d < 2.2).sum(-1)
+
+
+
+    sel = [ F.test(nodes=0, dir=0, min=1.5, max=3.5),
+            F.test(nodes=[0, 1], dir=0, min=1.5, max=3.5),
+            F.test(nodes=[0, 1, 2], dir=0, min=1.5, max=3.5),
+            F.test(nodes='all', dir=1, min=1.5, max=3.5),
+            F.test(nodes='any', dir=1, min=1.5, max=3.5),
+            F.test(nodes='none', dir=1, min=1.5),
+            (above > 0) * (below > 0 ),
+            close == 3,
+            ]
+
+    txt = [ 'First node has x between 1.5 and 3.5',
+            'First two nodes have x between 1.5 and 3.5',
+            'First 3 nodes have x between 1.5 and 3.5',
+            'All nodes have y between 1.5 and 3.5',
+            'Any node has y between 1.5 and 3.5',
+            'No node has y larger than 1.5',
+            'Touching the plane P = [3.2,3.0,0.0], n = [2.0,1.0,0.0]',
+            '3 nodes close to line through [0.0,0.0,0.0] and [1.0,1.0,1.0]',
+            ]
+
+
+
     draw(F)
 
     color = getcfg('canvas/colormap')[1:] # omit the black
