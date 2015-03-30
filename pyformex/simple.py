47c742879fe700d7dda324c1ca94e74fba9feffd
Francesco <francesco.iannaccone@ugent.be>
2015-03-30 23:46:31 +0200

    Fixed small import bug in simple.rectangle1, improved doc of simple.rectangleWithHole

--- pyformex/simple.py
+++ pyformex/simple.py
@@ -172,7 +172,7 @@ def rectangle1(L, W, nl, nw):
     - L,W: length,width of the rectangle.
 
     """
-    from Mesh import smartSeed
+    from pyformex.mesh import smartSeed, quadgrid
     sl = smartSeed(nl)
     sw = smartSeed(nw)
     return quadgrid(sl, sw).resized([L, W, 1.0])
