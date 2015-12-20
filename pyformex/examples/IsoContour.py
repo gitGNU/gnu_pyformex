1b36bbfe029edc2b6bd817bda923559fed63392b
Benedict Verhegghe <benedict.verhegghe@ugent.be>
2015-12-20 02:51:34 +0100

    Improve IsoContour example
    Lots of user options added to the IsoContour example

--- pyformex/examples/IsoContour.py
+++ pyformex/examples/IsoContour.py
@@ -36,20 +36,35 @@ from pyformex.gui.draw import *
 from pyformex.plugins.isosurface import isoline
 
 def run():
+    global filename # because we set it before updating globals
     resetAll()
     clear()
     # Set default image filename
     filename = getcfg('datadir')+'/butterfly.png'
     # Give user a change to change it
-    filename = askImageFile(filename)
+    viewer = widgets.ImageView(filename, maxheight=200)
+    res = askItems([
+        _I('filename', filename, text='Image file', itemtype='filename', filter='img', exist=True, preview=viewer),
+        viewer,
+        _I('value',None, choices=['luminance','intensity','red','green','blue']),
+        _I('n', 10, text='Number of isocontours'),
+        _I('levelmin', 0.0, text='Minimum level value'),
+        _I('levelmax', 1.0, text='Maximum level value'),
+        _I('npalette', 8, text='Number of different colors'),
+        _I('remove', False, text='Remove original image'),
+        ])
+
+    if not res:
+        return
+    globals().update(res)
+    print(res)
+    print(filename)
     # This is picked from pyformex.opengl.draw.drawImage3D
     from pyformex.plugins.imagearray import qimage2glcolor, resizeImage
     image = resizeImage(filename, 0, 0)
     nx, ny = image.width(), image.height()
     print("Image size: %s x %s" % (nx,ny))
     color, colortable = qimage2glcolor(image)
-    #butterfly nx,ny = 376,327
-    #zonnebloem nx,ny = 176,173
     color = color.reshape(ny,nx,3)
     #color = color[130:176,100:146] # uncomment to pick a part from the image
     ny,nx = color.shape[:2]   # pixels move fastest in x-direction!
@@ -58,20 +73,29 @@ def run():
     FA = draw(F, color=color, colormap=colortable, nolight=True)
     FA.alpha = 0.6
 
-    intens = FA.color.sum(axis=-1) / 3
-    data = intens.reshape(ny,nx)
+    if value == 'luminance':
+        data = luminance(FA.color)
+    elif value == 'intensity':
+        data = FA.color.sum(axis=-1) / 3
+    elif value == 'red':
+        data = FA.color[...,0]
+    elif value == 'green':
+        data = FA.color[...,1]
+    elif value == 'blue':
+        data = FA.color[...,2]
+    data = data.reshape(ny,nx)
 
-    n = 10
-    levels = arange(1,n) * 1. / n # change levels to adjust number and position of contours
-    print(levels)
+    levels = levelmin + arange(1,n) * (levelmax-levelmin) / n # change levels to adjust number and position of contours
+    #print(levels)
+    pf.canvas.settings.colormap = pf.refcfg.canvas.colormap[:npalette]
     transparent()
     for col,level in enumerate(levels):
         seg = isoline(data,level,nproc=-1)
         C = Formex(seg)
         draw(C,color=col+1,linewidth=3)
 
-    sleep(2)
-    undraw(FA)
+    if remove:
+        undraw(FA)
 
 if __name__ == '__draw__':
     run()
