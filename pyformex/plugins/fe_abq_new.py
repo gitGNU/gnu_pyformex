gitGNU/gnu_pyformex

commit 91fd7043a1eae50473534ad3670cda8c37de5aef

Improved fe_abq_new.exportMesh to save elements with different properties in different esets.
This function is copied from fe_abq_old. It works exactly as before, but now
allow to save diffrent element sets based on the mesh properties

pyformex/plugins/fe_abq_new.py
@@ -2832,26 +2832,34 @@ Script: %s
 ## Some convenience functions
 ##################################################
 
-def exportMesh(filename,mesh,eltype=None,header=''):
-    """Export a finite element mesh in Abaqus .inp format.
-
+def exportMesh(filename,mesh,eltype,header=''):
+ """Export a finite element mesh in Abaqus .inp format.
+    
     This is a convenience function to quickly export a mesh to Abaqus
     without having to go through the whole setup of a complete
     finite element model.
     This just writes the nodes and elements specified in the mesh to
-    the file with the specified name. The resulting file  can then be
-    imported in Abaqus/CAE or manual be edited to create a full model.
+    the file with the specified name. If the mesh has different properties,
+    the elements with the same properties, will be grouped in the same 
+    element set. The resulting file  can then be imported in Abaqus/CAE
+    or manual be edited to create a full model.
     If an eltype is specified, it will override the value stored in the mesh.
     This should be used to set a correct Abaqus element type matchin the mesh.
     """
     fil = open(filename, 'w')
     fil.write(fmtHeading(header))
-    if eltype is None:
-        eltype = mesh.eltype
     writeNodes(fil, mesh.coords)
-    writeElems(fil, mesh.elems, eltype, nofs=1)
+    if mesh.prop is None:
+        mesh = [mesh]
+    else:
+        mesh = mesh.splitProp(compact=False)
+    eofs = 1
+    for im,m in enumerate(mesh):
+        writeElems(fil, m.elems, eltype, name='ESET%s'%im, eofs=eofs, nofs=1)
+        eofs += m.nelems()
     fil.close()
     print("Abaqus file %s written." % filename)
+
 
 
 # End

commit ce9390a5977f8328bf91507c6123d1904cce1699

Fixed a bug introduced in previous commit 91fd704, in fe_abq_new

pyformex/plugins/fe_abq_new.py
@@ -2833,7 +2833,7 @@ Script: %s
 ##################################################
 
 def exportMesh(filename,mesh,eltype,header=''):
- """Export a finite element mesh in Abaqus .inp format.
+    """Export a finite element mesh in Abaqus .inp format.
     
     This is a convenience function to quickly export a mesh to Abaqus
     without having to go through the whole setup of a complete