47908a849b93165c44abcd1bf4f192c843f5130a
francesco <francesco.iannaccone@feops.com>
2016-06-23 14:52:35 +0200

    Fixed bug in connectivity. Class Adjacency was not imported, so methods using the class were failing.
    The class is now imported.

--- pyformex/connectivity.py
+++ pyformex/connectivity.py
@@ -36,6 +36,7 @@ from __future__ import print_function
 from pyformex import zip
 
 from pyformex.arraytools import *
+from pyformex.adjacency import Adjacency
 
 
 # BV: Should we make an InverseConnectivity class?
