gitGNU/gnu_pyformex

commit d19b4513db36170afd7940861f12270130434998

Add global variable to flag Python3
The global vaiable pf.PY3 is True if Python3 is used,
False if Python2 is used.
This makes it easier to add specific code.

pyformex/project.py
@@ -199,15 +199,15 @@ class Project(TrackedDict):
         signature: pyFormex ...
         contents: ['a', 'b', 'c', 'd', 'e']
       <BLANKLINE>
-      >>> print(utils.dictStr(P))
-      {'a': 1, 'b': 2, 'c': 3, 'd': [1, 2, 3], 'e': {'g': 5, 'f': 4}}
+      >>> print(utils.dictStr(P)) # doctest: +ELLIPSIS
+      {'a': 1, 'b': 2, 'c': 3, 'd': [1, 2, 3], 'e': ...}
       >>> P.save(quiet=True)
       >>> P.clear()
-      >>> print(dict.__str__(P))
+      >>> print(utils.dictStr(P))
       {}
       >>> P.load(quiet=True)
-      >>> print(utils.dictStr(P))
-      {'a': 1, 'b': 2, 'c': 3, 'd': [1, 2, 3], 'e': {'g': 5, 'f': 4}}
+      >>> print(utils.dictStr(P)) # doctest: +ELLIPSIS
+      {'a': 1, 'b': 2, 'c': 3, 'd': [1, 2, 3], 'e': ...}
 
     """
 
@@ -275,7 +275,11 @@ class Project(TrackedDict):
             #print("  Contents: %s" % self.keys())
         f = open(self.filename, 'w'+self.mode)
         # write header
-        f.write("%s\n" % self.header_data())
+        h = self.header_data()
+        print(h)
+        print(utils.dictStr(h))
+        header = "%s" % h
+        f.write(header)
         f.flush()
         if self.mode == 'b':
             # When using binary, can as well use highest protocol

pyformex/__init__.py
@@ -94,13 +94,17 @@ if sys.hexversion & 0xFFFF0000 > target_version:
     #print(startup_warnings)
 
 
-# Compatibility with Python2 and Python3
+# A single variable to flag Python 3
+PY3 = sys.hexversion >= 0x03000000
+
+# Compatibility between Python2 and Python3
 # We keep these in separate modules, because the ones for 2k might
 # not compile in 3k and vice-versa.
-if sys.hexversion < 0x03000000:
-    from pyformex.compat_2k import *
-else:
+
+if PY3:
     from pyformex.compat_3k import *
+else:
+    from pyformex.compat_2k import *
 
 
 #### Detect install type ########

pyformex/attributes.py
@@ -132,7 +132,7 @@ class Attributes(Dict):
 
 
     def __repr__(self):
-        return dict.__repr__(utils.removeDict(self, ['_default_dict_']))
+        return __str__(self)
 
 
 # End

pyformex/gui/pyconsole.py
@@ -30,11 +30,11 @@ from __future__ import print_function
 import sys
 import traceback
 
-
+import pyformex as pf
 from pyformex.gui import QtGui, QtCore
 from pyformex.gui.guimain import Board
 
-if sys.hexversion < 0x03000000:
+if not pf.PY3:
     range = xrange
 
 
@@ -61,7 +61,6 @@ parent=None):
         self.setUndoRedoEnabled(False)
         self.document().setDefaultFont(QtGui.QFont("monospace", 10, QtGui.QFont.Normal))
         self.showMessage(startup_message)
-
 
 
     def write(self,s,color=None):

pyformex/software.py
@@ -93,7 +93,7 @@ known_externals = {
     }
 
 
-if sys.hexversion >= 0x03000000:
+if pf.PY3:
     # Some detect commands fail on Python3
     del known_externals['calculix']
     del known_externals['postabq']

pyformex/utils.py
@@ -1016,12 +1016,12 @@ def moduleList(package='all'):
         modules = [ m for m in modules if not '.' in m ]
     elif package != 'all':
         modules = [ m for m in modules if m.startswith(package+'.') ]
-    if sys.hexversion < 0x03000000:
-        if 'compat_3k' in modules:
-            modules.remove('compat_3k')
-    else:
+    if pf.PY3:
         if 'compat_2k' in modules:
             modules.remove('compat_2k')
+    else:
+        if 'compat_3k' in modules:
+            modules.remove('compat_3k')
     return modules
 
 

pyformex/varray.py
@@ -69,6 +69,7 @@ by default, but provides the methods to sort them when needed.
 """
 from __future__ import print_function
 
+import pyformex as pf
 from pyformex import arraytools as at
 import numpy as np
 import sys
@@ -395,7 +396,7 @@ class Varray(object):
         self._row += 1
         return row
 
-    if (sys.hexversion) < 0x03000000:
+    if not pf.PY3:
         # In Python2 the next method is used instead of __next__
         next = __next__
 

commit a4e0bf50795945c36b9f843dd8ac9a2175fc172a

Fix a bug in project.py
The commit d19b451 broke the Project writing due to some
debugging output. Restored original code.

pyformex/project.py
@@ -276,9 +276,9 @@ class Project(TrackedDict):
         f = open(self.filename, 'w'+self.mode)
         # write header
         h = self.header_data()
-        print(h)
-        print(utils.dictStr(h))
-        header = "%s" % h
+        #print(h)
+        #print(utils.dictStr(h))
+        header = "%s\n" % h
         f.write(header)
         f.flush()
         if self.mode == 'b':