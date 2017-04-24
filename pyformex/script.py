gitGNU/gnu_pyformex

commit 70a663999946a19c69817fd9b7aed5e2e4c432db

Add PolyLine and BezierSpline to the pyFormex scripting language.
These two classes are widely used, stable and simple enough
for starting users.
Therefore they have been included in the core scripting language,
so that users do not have to import them.

pyformex/script.py
@@ -64,6 +64,7 @@ from pyformex.formex import *
 from pyformex.mesh import Mesh
 from pyformex.trisurface import TriSurface
 from pyformex.coordsys import CoordSys
+from pyformex.curve import PolyLine, BezierSpline
 from pyformex import simple
 
 

commit 5997f6768da3044d7066847c8802124227c707a3

Fix a fatal import bug introduced in 70a663999

pyformex/script.py
@@ -64,7 +64,7 @@ from pyformex.formex import *
 from pyformex.mesh import Mesh
 from pyformex.trisurface import TriSurface
 from pyformex.coordsys import CoordSys
-from pyformex.curve import PolyLine, BezierSpline
+from pyformex.plugins.curve import PolyLine, BezierSpline
 from pyformex import simple
 
 